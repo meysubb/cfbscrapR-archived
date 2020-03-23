857b97e6e809e0c1d9bc8cb60d6d96da835e91fa
Meyappan <meysubb@gmail.com>
2020-03-22 22:00:37 -0500

    srs ranks

---
+++ R/cfb_srs_ranking.R
@@ -0,0 +1,47 @@
+#' Get SRS historical rating data
+#'
+#'
+#' @param year Year (optional)
+#' @param team Team information (optional)
+#' @param confefence Conference Abbreivation - S&P+ information by confderence
+#'
+#' @keywords internal
+#' @importFrom jsonlite "fromJSON"
+#' @importFrom assertthat "assert_that"
+#' @export
+#' @examples
+#'
+#' cfb_srs_ranking(year=2019)
+#'
+#' cfb_srs_ranking(team='Texas A&M')
+#'
+#' cfb_srs_ranking(2019,team="Texas")
+#'
+#' cfb_srs_ranking(conference='SEC')
+#'
+#' cfb_srs_ranking(year=2018,conference='SEC')
+
+
+cfb_srs_ranking <- function(year=NULL,team=NULL,conference=NULL){
+  if(is.null(conference )){
+    base_url = "https://api.collegefootballdata.com/ratings/srs"
+    full_url = paste0(base_url,"?year=",year)
+    if (!is.null(team)){
+      team = URLencode(team, reserved = T)
+      full_url = paste0(full_url,'&team=',team)
+    }
+    df = fromJSON(full_url)
+    return(df)
+  }
+  if(!is.null(conference)){
+    browser()
+    conf_check = conference %in% cfb_conf_types_df$abbreviation
+    assert_that((conf_check) == T, msg = "Please provide an appropriate conference abberivation.")
+    base_url = 'https://api.collegefootballdata.com/ratings/srs/conferences'
+    assert_that(is.null(year) == T, msg = "Please provide a year along with the conference")
+    full_url = paste0(base_url,"?year=",year,"&conference=",conference)
+    df = fromJSON(full_url)
+    return(df)
+  }
+
+}
